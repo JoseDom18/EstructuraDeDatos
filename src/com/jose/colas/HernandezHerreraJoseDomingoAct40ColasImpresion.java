bbdf5da11c26f6446b7388fcd23e0cce264699be
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-12-08 12:23:50 -0600

    ejercico 40 empezado

---
+++ src/com/jose/colas/HernandezHerreraJoseDomingoAct40ColasImpresion.java
@@ -0,0 +1,53 @@
+package com.jose.colas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HernandezHerreraJoseDomingoAct40ColasImpresion {
+
+	public static void main(String[] args) {
+		
+		File f1 = new File();
+		File f2 = new File();
+		System.out.println(f1);
+		System.out.println(f2);
+		List<File> archivero = new ArrayList<File>();
+		for (int i = 0; i < 10; i++) {
+			File f = new File();
+			archivero.add(f);
+		}
+		
+
+	}
+	
+	
+	private static class File {
+		private String name;
+		private int pageNumber;
+		private int size;
+		private int sizeMb;
+		private static int id = 1;
+		
+		
+		public File() {
+			String[] ext = {".doc", ".ppt", ".xls", ".txt", ".pdf"};
+			this.name = "Archivo" + String.format("%03d", id) + ext[new Random().nextInt(5)];
+			this.pageNumber = new Random().nextInt(50) + 1;
+			this.size = new Random().nextInt(48001) + 2000;
+			this.sizeMb = this.size / 1024;
+			id++;
+		}
+
+
+		@Override
+		public String toString() {
+			String formatoTabla = "%-25s %-15s %-15s%n";
+			String kb = String.format("%dKb", size);
+			String mb = String.format("%dMb", sizeMb);
+			String s = String.format(formatoTabla, name, kb, mb);
+			return s;
+		}
+	}
+
+}
