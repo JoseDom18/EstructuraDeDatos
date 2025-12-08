55076f5b1a3ba7b059ba72d29dc392ac7725bea0
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-12-08 08:09:29 -0600

    cabios

--- src/com/jose/colas/Colas.java
+++ src/com/jose/colas/Colas.java
@@ -3,6 +3,9 @@ package com.jose.colas;
 public class Colas {
 
 	public static void main(String[] args) {
+		
+		String formato = "%03d %03d";
+		System.out.printf(formato, 2);
 
 		// Queue<Clase> cola = new LinkedList<Clase>();	
 
